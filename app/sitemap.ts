5df3d8a741e0c961cf41c17a35bda52388dd040a
hrishikesh <netke2611@gmail.com>
2025-12-28 16:52:59 +0530

    .

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export const dynamic = 'force-static'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://talentflip.com'
+  const baseUrl = 'https://talentflip.ai'
   
   return [
     {
